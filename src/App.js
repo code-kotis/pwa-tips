6d4c1c1317605d163251c22cdc486f96d46077f9
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2016-10-22 13:25:17 +0530

    Updated site

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  openSignup() {
+    require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us11.list-manage.com","uuid":"7882ec66041fdf12fb314f2fc","lid":"b820ab6def"}) })
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,7 +18,7 @@ class App extends Component {
 						<a href="https://pwa.tips/codelabs">Codelabs</a>
         	</div>
           <div className="App__card">
-          	<a href="https://pwa.tips">PWA Tips</a>
+          	<a href="http://eepurl.com/clf6S5" target="_blank">PWA Tips</a>
           </div>
         </div>
       </div>
