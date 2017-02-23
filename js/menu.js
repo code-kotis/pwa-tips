2a4f1fcf061c1d33614c8d59d12846b352036335
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2017-02-23 19:08:18 +0530

    Added mobile menu

--- js/menu.js
+++ js/menu.js
@@ -18,6 +18,7 @@
 
   //To hide menu
   function hideMenu() {
+    menuElement.style = "";
     menuElement.classList.remove("menu--show");
     menuOverlayElement.classList.remove("menu__overlay--show");
     menuElement.addEventListener("transitionend", onTransitionEnd, false);
