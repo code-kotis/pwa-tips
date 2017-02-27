0fd0bd44c044c4ebde8767579114a54a261c7472
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2017-02-27 15:17:12 +0530

    Added form submit

--- js/main.js
+++ js/main.js
@@ -7,3 +7,28 @@ if ("serviceWorker" in navigator && window.location.hostname !== "localhost") {
       console.log("Failed to register serviceWorker", error);
     });
 }
+
+
+var submitBtn = document.getElementById("submit");
+var submitModal = document.getElementById("modal");
+var submitModalOverlay = document.getElementById("modal__overlay");
+
+submitBtn.addEventListener("click", function () {
+  toggleModal();
+});
+
+submitModalOverlay.addEventListener("click", function () {
+  toggleModal();
+});
+
+function toggleModal() {
+  var isHidden = submitModal.classList.contains("modal__hide");
+  if (isHidden) {
+    submitModal.classList.remove("modal__hide");
+    document.body.style.overflow = "hidden";
+  }
+  else {
+    submitModal.classList.add("modal__hide");
+    document.body.style.overflow = "hidden";
+  }
+}
