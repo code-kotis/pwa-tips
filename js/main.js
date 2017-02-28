e2a1133a1a48eb3386def4d65426048a0780a067
Gokulakrishnan Kalaikovan <krishnangokul9@gmail.com>
2017-02-28 19:21:56 +0530

    Updated

--- js/main.js
+++ js/main.js
@@ -12,23 +12,27 @@ if ("serviceWorker" in navigator && window.location.hostname !== "localhost") {
 var submitBtn = document.getElementById("submit");
 var submitModal = document.getElementById("modal");
 var submitModalOverlay = document.getElementById("modal__overlay");
+var myForm = document.getElementById("myForm");
+var formURL = "https://docs.google.com/forms/d/e/1FAIpQLScySacoFq68OYaX9EvLpyP2H4EO7vBeNDPkxlrxEyM-gN8YgA/viewform?embedded=true";
 
 submitBtn.addEventListener("click", function () {
   toggleModal();
 });
 
-submitModalOverlay.addEventListener("click", function () {
+submitModalOverlay && submitModalOverlay.addEventListener("click", function () {
   toggleModal();
 });
 
 function toggleModal() {
   var isHidden = submitModal.classList.contains("modal__hide");
   if (isHidden) {
+    myForm.src = formURL;
     submitModal.classList.remove("modal__hide");
     submitModalOverlay.classList.remove("modal__hide");
     document.body.style.overflow = "hidden";
   }
   else {
+    myForm.src = "";
     submitModal.classList.add("modal__hide");
     submitModalOverlay.classList.add("modal__hide");
     document.body.style.overflow = "";
